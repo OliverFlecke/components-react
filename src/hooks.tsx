919ac0f4565bbf7c1e2854dd9a2c2a4b86d3db26
Oliver Fleckenstein <oliverfl@live.dk>
2021-05-03 21:49:18 +0200

    v0.1.5

--- src/hooks.tsx
+++
@@ -1,4 +0,0 @@
-import useDarkMode from './utils/useDarkMode';
-import useId from './utils/useId';
-
-export { useDarkMode, useId };
--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,7 @@ import Button from './Button';
 import ButtonContainer from './ButtonContainer';
 import Input from './Input';
 import Modal from './Modal';
+import useDarkMode from './utils/useDarkMode';
+import useId from './utils/useId';
 
-export { Button, ButtonContainer, Input, Modal };
+export { Button, ButtonContainer, Input, Modal, useDarkMode, useId };
