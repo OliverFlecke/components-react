82aadde46ac226302f9a8434c29918da0bb2de4f
Oliver Fleckenstein <oliverfl@live.dk>
2021-12-26 16:26:05 +0100

    fix: removed invalid properties from DarkModeToggle

--- src/DarkModeToggle.tsx
+++ src/DarkModeToggle.tsx
@@ -1,44 +1,40 @@
 import React, { FC } from 'react';
 
-const Icon: FC<{ isDark: boolean }> = ({ isDark }) => {
-	if (isDark) {
-		return (
-			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-				<path
-					d="M160 136c0-30.62 4.51-61.61 16-88C99.57 81.27 48 159.32 48 248c0 119.29 96.71 216 216 216 88.68 0 166.73-51.57 200-128-26.39 11.49-57.38 16-88 16-119.29 0-216-96.71-216-216z"
-					fill="none"
-					stroke="currentColor"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="32"
-				/>
-			</svg>
-		);
-	} else {
-		return (
-			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-				<path
-					fill="none"
-					stroke="currentColor"
-					stroke-linecap="round"
-					stroke-miterlimit="10"
-					stroke-width="32"
-					d="M256 48v48M256 416v48M403.08 108.92l-33.94 33.94M142.86 369.14l-33.94 33.94M464 256h-48M96 256H48M403.08 403.08l-33.94-33.94M142.86 142.86l-33.94-33.94"
-				/>
-				<circle
-					cx="256"
-					cy="256"
-					r="80"
-					fill="none"
-					stroke="currentColor"
-					stroke-linecap="round"
-					stroke-miterlimit="10"
-					stroke-width="32"
-				/>
-			</svg>
-		);
-	}
-};
+const Moon = () => (
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+		<path
+			d="M160 136c0-30.62 4.51-61.61 16-88C99.57 81.27 48 159.32 48 248c0 119.29 96.71 216 216 216 88.68 0 166.73-51.57 200-128-26.39 11.49-57.38 16-88 16-119.29 0-216-96.71-216-216z"
+			fill="none"
+			stroke="currentColor"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			strokeWidth="32"
+		/>
+	</svg>
+);
+
+const Sun = () => (
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+		<path
+			fill="none"
+			stroke="currentColor"
+			strokeLinecap="round"
+			strokeMiterlimit="10"
+			strokeWidth="32"
+			d="M256 48v48M256 416v48M403.08 108.92l-33.94 33.94M142.86 369.14l-33.94 33.94M464 256h-48M96 256H48M403.08 403.08l-33.94-33.94M142.86 142.86l-33.94-33.94"
+		/>
+		<circle
+			cx="256"
+			cy="256"
+			r="80"
+			fill="none"
+			stroke="currentColor"
+			strokeLinecap="round"
+			strokeMiterlimit="10"
+			strokeWidth="32"
+		/>
+	</svg>
+);
 
 export interface DarkModeToggleProps {
 	darkMode: boolean;
@@ -46,17 +42,17 @@ export interface DarkModeToggleProps {
 }
 
 const DarkModeToggle: FC<DarkModeToggleProps> = ({
-	darkMode: isDarkMode,
+	darkMode,
 	onToggle,
 }: DarkModeToggleProps) => (
 	<button
 		onClick={onToggle}
 		className={`h-6 w-6 focus:outline-none ${
-			isDarkMode ? 'text-white' : 'text-black'
+			darkMode ? 'text-white' : 'text-black'
 		}`}
 		aria-label="theme toggle"
 	>
-		<Icon isDark={isDarkMode} />
+		{darkMode ? <Moon /> : <Sun />}
 	</button>
 );
 
