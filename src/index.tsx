4326083b6c13700db9758a597875fa96bd27617c
Oliver Fleckenstein <oliverfl@live.dk>
2021-12-26 21:09:39 +0100

    fix: exporting select component

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import Button from './Button';
 import ButtonContainer from './ButtonContainer';
+import DarkModeToggle from './DarkModeToggle';
 import Input from './Input';
 import Modal from './Modal';
-import DarkModeToggle from './DarkModeToggle';
+import Select from './Select';
 import useDarkMode from './utils/useDarkMode';
 import useDarkModeWithClass from './utils/useDarkModeWithClass';
 import useId from './utils/useId';
@@ -11,6 +12,7 @@ export {
 	Button,
 	ButtonContainer,
 	Input,
+	Select,
 	Modal,
 	DarkModeToggle,
 	useDarkMode,
