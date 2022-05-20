cbd177764dad692f04af94545b41be8d3968428a
Oliver Fleckenstein <oliverfl@live.dk>
2022-05-21 00:21:58 +0100

    refactor!: Changed Modal to use React portals
    BREAKING CHANGES: This changes the way modals are rendered in the DOM, which should not cause any issues but are still considered a breaking change

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -1,4 +1,5 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useMemo } from 'react';
+import ReactDOM from 'react-dom';
 
 export interface ModalProps {
 	children: any;
@@ -6,24 +7,63 @@ export interface ModalProps {
 	onDismiss?: () => void;
 }
 
-const Modal: FC<ModalProps> = ({ isOpen, children, onDismiss }: ModalProps) => {
-	if (!isOpen) return null;
+const Modal: FC<ModalProps> = props => {
+	if (!props.isOpen) return null;
 
-	return (
-		<div className="absolute top-0 left-0 w-screen h-screen">
-			<div
-				className="h-full max-w-full center bg-opacity-50 bg-white dark:bg-opacity-50 dark:bg-black"
-				onClick={onDismiss}
-			>
-				<div
-					className="bg-white dark:bg-black rounded-md m-4"
-					onClick={e => e.stopPropagation()}
-				>
-					{children}
-				</div>
-			</div>
-		</div>
-	);
+	return <Container {...props} />;
 };
 
 export default Modal;
+
+const Container: FC<ModalProps> = ({ children, onDismiss }) => {
+	const { node } = useNodeInRoot('modal-root', {
+		className: 'absolute top-0 left-0 w-screen h-screen',
+	});
+
+	const render = (
+		<div
+			className="h-full max-w-full center bg-opacity-50 bg-white dark:bg-opacity-50 dark:bg-black"
+			onClick={onDismiss}
+		>
+			<div
+				className="bg-white dark:bg-black rounded-md m-4"
+				onClick={e => e.stopPropagation()}
+			>
+				{children}
+			</div>
+		</div>
+	);
+
+	return ReactDOM.createPortal(render, node);
+};
+
+function getOrCreateRoot(id: string): HTMLElement {
+	let root = document.getElementById(id);
+
+	if (!root) {
+		const body = document.getElementsByTagName('body')[0];
+		root = document.createElement('div');
+		root.id = id;
+		body.appendChild(root);
+	}
+
+	return root;
+}
+
+function useNodeInRoot(id: string, options?: unknown) {
+	const node = useMemo(() => document.createElement('div'), []);
+	if (options) {
+		Object.assign(node, options);
+	}
+
+	useEffect(() => {
+		const root = getOrCreateRoot(id);
+		root.appendChild(node);
+
+		return () => {
+			root?.removeChild(node);
+		};
+	}, [node, id]);
+
+	return { node };
+}
